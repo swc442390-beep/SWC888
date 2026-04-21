78018352b59a02e900f5916d098e17a0db3ad207
swc442390-beep <swc442390@gmail.com>
2026-04-22 07:01:21 +0800

    working with env for db

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: 'postgresql://gamedb_qwiu_user:fs8wqx8FgcSp7BFxqJeZUPZybaw3nfnr@dpg-d76vih2a214c73d6g450-a.oregon-postgres.render.com/gamedb_qwiu',
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
   }
